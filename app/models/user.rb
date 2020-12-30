936ad178395c6721f9a50da8040b3310d249cd5a
mamoru-ando <bsirregulars@yahoo.co.jp>
2020-12-30 09:05:07 +0900

    ユーザー新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   has_many :items
   has_many :comments
   has_many :orders
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
