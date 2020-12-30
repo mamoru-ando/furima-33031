9ba08dd50063e75af6474737a2bffe176cc68aa3
mamoru-ando <bsirregulars@yahoo.co.jp>
2020-12-30 15:57:18 +0900

    モデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,11 @@ class User < ApplicationRecord
   has_many :orders
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナ文字を使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナ文字を使用してください' }
   validates :birthday, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
 end
