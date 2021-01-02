d0bc7b1afad327304c828d2ace4266e762e6979d
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-02 22:29:15 +0900

    モデル単体テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name              {"商品名"}
+    text              {"商品説明"}
+    category_id       {Faker::Number.within(range: 1..10)}
+    status_id         {Faker::Number.within(range: 1..6)}
+    delivery_id       {Faker::Number.within(range: 1..2)}
+    prefecture_id     {Faker::Number.within(range: 1..47)}
+    delivery_days_id  {Faker::Number.within(range: 1..3)}
+    price             {1000} 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
