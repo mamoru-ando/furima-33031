daf63579e7518f7da7db2f031f1f6998a03330cf
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-02 15:08:45 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery
   belongs_to_active_hash :delivery_days
 
-  # validates :image, :name, :text, :price, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum 40 }
+    validates :text, length: { maximum 1000 }
+    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
+  end
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :prefecture_id
+    validates :category_id
+    validates :status_id
+    validates :delivery_id
+    validates :delivery_days_id
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
 
-  # validates :prefecture_id, numericality: { other_than: 1 } 
 end
