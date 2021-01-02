914b941544762b4e29622e1fe46c159f8631e3b7
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-02 14:32:55 +0900

    ActiveHashの導入と出品ボタン遷移先の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(iteme_params)
+  end
+
+  private
+
+  def iteme_params
+    params.require(:item).permit(:image, :name, :text, :price, :category_id, :status_id, :delivery_id, :delivery_days_id, :prefecture_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :delivery_days
+
+  # validates :image, :name, :text, :price, presence: true
+
+  # validates :prefecture_id, numericality: { other_than: 1 } 
 end
