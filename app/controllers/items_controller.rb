e72e95fd1001d9f985d10cb49c7bf7b1c3ef1fed
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-05 11:03:09 +0900

    編集した情報が更新される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
@@ -27,6 +27,15 @@ class ItemsController < ApplicationController
   def edit
     # binding.pry
     @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
