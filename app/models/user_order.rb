f32b0fc0453a5d92050d37e8c9e99b6bfc4dadbf
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-05 16:11:13 +0900

    Formオブジェクト、モデルの作成とアソシエーション

---
+++ app/models/user_order.rb
@@ -0,0 +1,20 @@
+class UsreOrder
+  include ActiveModel::Model
+  attr_accessor :user, :item, :postcode, :prefecture_id, :city, :blok, :building, :phone_number, :order
+
+  with_options presence: true do
+    validates :token
+    validates :postcode, numericality: { with: ^\d{3}-\d{4}$, message: "ハイフンを入れて下さい"}
+    validates :city
+    validates :blok
+    validates :phone_number, numericality: { with: ^0\d{9,10}$, message: "can't be blank" }
+  end
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    # 住所の情報を保存
+    Address.create(postcode: postcode, prefecture: prefecture, city: city, blok: blok, building: building, user_id: user.id, order_id: order.id)
+    # 寄付金の情報を保存
+    Order.create(item: item, user_id: user.id)
+  end
+end
