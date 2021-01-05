989387b442ca7451586027f292ee00497ec0b502
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-05 19:13:41 +0900

    購入画面の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,10 @@
 class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @user_order = UserOrder.new
+  end
+
+  def create
+  end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,13 +1,13 @@
-class UsreOrder
+class UserOrder
   include ActiveModel::Model
   attr_accessor :user, :item, :postcode, :prefecture_id, :city, :blok, :building, :phone_number, :order
 
   with_options presence: true do
     validates :token
-    validates :postcode, numericality: { with: ^\d{3}-\d{4}$, message: "ハイフンを入れて下さい"}
+    validates :postcode #, numericality: { with: ^\d{3}-\d{4}$, message: "ハイフンを入れて下さい"}
     validates :city
     validates :blok
-    validates :phone_number, numericality: { with: ^0\d{9,10}$, message: "can't be blank" }
+    validates :phone_number #, numericality: { with: ^0\d{9,10}$, message: "can't be blank" }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
