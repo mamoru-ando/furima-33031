034395a425eb146a36c4334bddfc0fb5b8b6022f
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-06 17:46:12 +0900

    購入情報の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,5 +6,22 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+    # binding.pry
+      @user_order.save
+      redirect_to root_path
+    else
+    #  binding.pry
+      render :index
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:user_order).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number)
+                                  .merge(user_id: current_user.id, item_id: @item.id)
+    end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,20 +1,22 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :user, :item, :postcode, :prefecture_id, :city, :blok, :building, :phone_number, :order
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
 
   with_options presence: true do
-    validates :token
-    validates :postcode #, numericality: { with: ^\d{3}-\d{4}$, message: "ハイフンを入れて下さい"}
+    # validates :token
+    validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: "ハイフンを入れて下さい"}
     validates :city
-    validates :blok
-    validates :phone_number #, numericality: { with: ^0\d{9,10}$, message: "can't be blank" }
+    validates :block
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "ハイフンなしで入力" }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
+    binding.pry
+    # 購入の情報を保存
+    order = Order.create(item_id: item_id, user_id: user_id)
     # 住所の情報を保存
-    Address.create(postcode: postcode, prefecture: prefecture, city: city, blok: blok, building: building, user_id: user.id, order_id: order.id)
-    # 寄付金の情報を保存
-    Order.create(item: item, user_id: user.id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block,
+                   building: building, phone_number: phone_number, order_id: order.id)
   end
 end
