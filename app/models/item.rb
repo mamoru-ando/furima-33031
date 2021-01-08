86e8e6f72d9e51f48f9a3da19c5157801062a3ac
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-08 09:40:33 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,9 +13,9 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :text, length: { maximum: 1000 }
-    validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters.' }
+    validates :price, numericality: { only_integer: true, message: 'は半角で入力してください' }
   end
-  with_options numericality: { other_than: 0, message: "can't be blank" } do
+  with_options numericality: { other_than: 0, message: "を選択してください" } do
     validates :prefecture_id
     validates :category_id
     validates :status_id
@@ -23,5 +23,5 @@ class Item < ApplicationRecord
     validates :delivery_days_id
   end
   validates :price,
-            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は範囲内に設定してください' }
 end
